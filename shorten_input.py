b29c1b8d9cb48024d53c9eb530d1ca10c95be2a3
sakum20 <sathishkmr460@gmail.com>
2024-02-02 22:08:08 +0530

    cm1

--- shorten_input.py
+++ shorten_input.py
@@ -3,11 +3,26 @@ class construct_item():
         self.item_name = item 
     
     def print_original_item(self):
-        print(self.item_name)
+        print(f'Original items: {self.item_name}')
         
 class shorten_itemANDchar(construct_item):
     def shorten(self):
-        print(self.item_name[0:3])
+        print(f'Shortened items: {self.item_name[0:3]}')
+
+
+class shorten_dict(construct_item):
+    def shorten(self):
+        dict = self.item_name
+        counter = 0
+        result_dict = {}
+
+        for (k, v) in dict.items():
+            if(counter < 3):
+                result_dict.update({k: v})
+                counter = counter + 1
+        print(f'Shortened item: {result_dict}')
+
+
 
 item1 = shorten_itemANDchar("Hello")
 item1.print_original_item()
@@ -16,3 +31,8 @@ item1.shorten()
 item2 = shorten_itemANDchar([1,2,3,4])
 item2.print_original_item()
 item2.shorten()
+
+
+my_dict = shorten_dict({1: 'mike', 2: 'satz', 3: 'nariya', 4: 'leo', 5: 'namdi'})
+my_dict.print_original_item()
+my_dict.shorten()
